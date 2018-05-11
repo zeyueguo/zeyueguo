0212774019ca095ffb217ef016404af5d124c9e4
麦砚 <maiyan@ehousechina.com>
2018-05-11 14:58:59 +0800

    保存提交

---
+++ books/SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+* [Introduction](README.md)
+* [Chapter1](chapter1/README.md)
+  * [Section1.1](chapter1/section1.1.md)
+
---
+++ books/_book/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source=../public/books
+for dir in *;
+do
+  #删除gitbook生成的内容
+  if [ -d $source/$dir ]; then
+    rm -r $source/$dir
+  fi
+  #重新生成gitbook的内容
+  if [ -d $dir ]; then
+    gitbook build $dir $source/$dir
+  fi
+done
